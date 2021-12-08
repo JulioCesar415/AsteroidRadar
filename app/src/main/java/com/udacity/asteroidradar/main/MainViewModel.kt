890dc238c5a6adc42121c06fbfc9181e00c62d4a
JulioCesar415 <35044318+JulioCesar415@users.noreply.github.com>
2021-12-08 15:31:10 -0800

    pass getAsteroids to asteroids

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,12 @@
 package com.udacity.asteroidradar.main
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.udacity.asteroidradar.database.AsteroidDatabaseDao
 
-class MainViewModel : ViewModel() {
+class MainViewModel(
+    val database: AsteroidDatabaseDao, application: Application) : AndroidViewModel(application) {
+
+//    define asteroids, then getAllAsteroids() from database
+    val asteroids = database.getAsteroids()
 }
