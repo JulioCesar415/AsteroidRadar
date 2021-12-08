737bbb317bb8f78609bd9b4d58b6072d998f70b6
JulioCesar415 <35044318+JulioCesar415@users.noreply.github.com>
2021-12-08 15:32:36 -0800

    implement adapter and observe for asteroid change

--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -2,23 +2,59 @@ package com.udacity.asteroidradar.main
 
 import android.os.Bundle
 import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.udacity.asteroidradar.R
+import com.udacity.asteroidradar.database.AsteroidDatabase
+import com.udacity.asteroidradar.database.DatabaseEntities
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
-    private val viewModel: MainViewModel by lazy {
-        ViewModelProvider(this).get(MainViewModel::class.java)
-    }
+//    private val viewModel: MainViewModel by lazy {
+//        ViewModelProvider(this).get(MainViewModel::class.java)
+//    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding = FragmentMainBinding.inflate(inflater)
+//        val binding = FragmentMainBinding.inflate(inflater)
+
+//        bind to resource layout file
+        val binding: FragmentMainBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_main, container, false
+        )
+
+//        get reference to application
+        val application = requireNotNull(this.activity).application
+//        get reference to DAO of database
+        val dataSource = AsteroidDatabase.getInstance(application).asteroidDatabaseDao
+//        create instance of viewModelFactory
+        val viewModelFactory = MainViewModelFactory(dataSource, application)
+
+//        reference to MainViewModel
+        val viewModel = ViewModelProvider(
+            this, viewModelFactory
+        ).get(MainViewModel::class.java)
+
+//        pass mainViewModel binding variable to mainViewModel
+        binding.viewModel = viewModel
+
+//        set current activity as the lifecycle owner of the binding
         binding.lifecycleOwner = this
 
-        binding.viewModel = viewModel
+//        tell RecyclerView about adapter.
+        val adapter = AsteroidMainAdapter()
+//        use adapter to display views on the screen
+        binding.asteroidRecycler.adapter = adapter
+
+//        tell adapter what data it should adapt
+        viewModel.asteroids.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.data = it
+            }
+        })
 
         setHasOptionsMenu(true)
 
